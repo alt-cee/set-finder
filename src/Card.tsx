ced65295ae3038798661010290d761e26695b7b2
ai-lien <a.trancong@gmail.com>
2023-06-15 16:24:47 -0700

    add blank state to card

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,27 +1,34 @@
 import { ReactComponent as Diamond } from './assets/diamond.svg'
 import { ReactComponent as Oval } from './assets/oval.svg'
 import { ReactComponent as Tilde } from './assets/tilde.svg'
+import { useState } from 'react';
+
+class CardFace {
+    constructor(
+        color: string | null, 
+        fill: string | null,
+        shape: ReactComponent | null,
+        count: number | null) {
+        this.color = color; // {red, purple, green}
+        this.fill = fill; // {blank, stripe, solid}
+        this.shape = shape; // {oval, diamond, tilde}
+        this.count = count; // {1, 2, 3}
+    }
+    // TODO: add public method for callees to determine if null or not
+}
 
 const Card = (props) => {
     const top = props.position.top;
     const left = props.position.left;
-    class CardFace {
-        constructor(
-            color: string, 
-            fill: string,
-            shape: ReactComponent,
-            count: number) {
-            this.color = color; // {red, purple, green}
-            this.fill = fill; // {blank, stripe, solid}
-            this.shape = shape; // {oval, diamond, tilde}
-            this.count = count; // {1, 2, 3}
-        }
-    }
-    const card = new CardFace("purple", "url(#diagonalHatch)", Oval, 2)
+    const [card, setCard] = useState(new CardFace(null, null, null, null));
+    //const card = new CardFace("purple", "url(#diagonalHatch)", Oval, 2)
 
     function Shape({card}) {
+        if (card.color == null || card.fill == null || card.shape == null || card.count == null) {
+            return <p>Empty!</p>
+        }
         const CardShape=card.shape; 
-        return <CardShape color={card.color} fill={card.fill}/>;
+        return <CardShape color={card.color} fill={card.fill} />
     }
 
     //TODO: improve the shape repetition
