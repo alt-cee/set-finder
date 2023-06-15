ca03f39bde234b38262ba90dec888120d075dfe7
ai-lien <a.trancong@gmail.com>
2023-06-15 11:41:18 -0700

    render card face based on state stored in Card

--- src/Card.tsx
+++ src/Card.tsx
@@ -5,13 +5,20 @@ import { ReactComponent as Tilde } from './assets/tilde.svg'
 const Card = (props) => {
     const top = props.position.top;
     const left = props.position.left;
+    const cardColor = "purple"; // {red, purple, green}
+    const cardFill = "url(#diagonalHatch)"; // {blank, stripe, solid}
+    const cardShape = Diamond; // {oval, diamond, tilde}
+    const cardCount = 3; // {1, 2, 3}
 
+    function Shape() {
+        const CardShape=cardShape; 
+        return <CardShape color={cardColor} fill={cardFill}/>;
+    }
+    
     return (
         <div className="card" style={{'top': top, 'left': left}}>
             <div className="card-face">
-                <Tilde />
-                <Diamond />
-                <Oval />
+                {[...Array(cardCount)].map((value) => (Shape()))}
             </div>
 
         </div>
